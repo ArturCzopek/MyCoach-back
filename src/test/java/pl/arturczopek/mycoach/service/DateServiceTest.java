e8441d074bf604992cbc86fe0a4b92949feb8eab
Artur Czopek <arturcz32@gmail.com>
2017-04-06 18:51:19 +0200

    Added dummy test

---
+++ src/test/java/pl/arturczopek/mycoach/service/DateServiceTest.java
@@ -0,0 +1,15 @@
+package pl.arturczopek.mycoach.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DateServiceTest {
+
+    @Test
+    public void helloTest() {
+        int numberOne = 1;
+        int numberTwo = 2;
+        assertEquals(numberOne + numberTwo, 3);
+    }
+}
